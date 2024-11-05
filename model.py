37380bc4ab0a93629ba070ebb2d8e3288e7194c1
LeonidU <leoniduroshlev@gmail.com>
2024-11-06 01:21:13 +0300

    Fix NN model

--- model.py
+++ model.py
@@ -2,10 +2,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from sklearn.metrics import roc_auc_score
 import cell_sampling
+import numpy as np
 # Define the 1D CNN model
 class CNN1DClassifier(nn.Module):
-    def __init__(self, input_length, num_classes=21):
+    def __init__(self, input_length, num_classes):
         super(CNN1DClassifier, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
@@ -18,7 +20,7 @@ class CNN1DClassifier(nn.Module):
         conv_output_length = input_length // 8
         self.fc1 = nn.Linear(64 * conv_output_length, 128)
         self.fc2 = nn.Linear(128, num_classes)
-        self.softmax = nn.Softmax(dim=1)
+#        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = x.unsqueeze(1)  # Add channel dimension
@@ -31,11 +33,16 @@ class CNN1DClassifier(nn.Module):
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
-        x = self.softmax(x)
+#        x = self.softmax(x)
+#        print("Iteration over")
         return x
 
 
 # Initialize the model, loss function, and optimizer
+if torch.cuda.is_available():
+    print("CUDA GPU is available.")
+else:
+    print("CUDA GPU is not available.")
 
 mtx_path = "../E-ANND-2/E-ANND-2.aggregated_filtered_normalised_counts.mtx"
 colnames_path = "../E-ANND-2/E-ANND-2.aggregated_filtered_normalised_counts.mtx_cols"
@@ -45,9 +52,13 @@ features_path = "Hsapiens_features.txt"
 num_classes, input_length, dataset = cell_sampling.load_data(mtx_path, colnames_path, cells_path, rownames_path, features_path)
 print(num_classes)
 print(input_length)
-dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 model = CNN1DClassifier(input_length=input_length,  num_classes=num_classes)
+model = model.to(device)
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -56,10 +67,12 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs=20):
     model.train()
     for epoch in range(num_epochs):
         running_loss = 0.0
+        all_labels = []
+        all_outputs = []
         for inputs, labels in dataloader:
             # Move data to the device (GPU or CPU)
-            inputs, labels = inputs.float(), labels.long()
-
+#            inputs, labels = inputs.float(), labels
+            inputs, labels = inputs.float().to(device), labels.long().to(device)
             # Zero the gradient
             optimizer.zero_grad()
 
@@ -70,13 +83,16 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs=20):
             # Backward pass and optimization
             loss.backward()
             optimizer.step()
-
-            # Update loss
-            running_loss += loss.item() * inputs.size(0)
+            all_labels.extend(labels.cpu().numpy())
+            all_outputs.extend(outputs.cpu().detach().numpy())
+            running_loss += loss.item()
 
         epoch_loss = running_loss / len(dataloader.dataset)
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}')
-
-
+        print(f'Epoch {epoch+1}, Loss: {epoch_loss:.4f}')
+        all_outputs_np = np.array(all_outputs)
+        all_labels_np = np.array(all_labels)
+        auc = roc_auc_score(all_labels_np, all_outputs_np, multi_class='ovr')
+        print(f'AUC {auc}')
+    torch.save(model.state_dict(), "convnet1d_model.pth")
 
 train_model(model, dataloader, criterion, optimizer, num_epochs=100)
