482c3623ce212b1d157edc89cd39447c5bbec273
LeonidU <leoniduroshlev@gmail.com>
2024-11-06 17:24:39 +0300

    Fix bugs

--- model.py
+++ model.py
@@ -13,15 +13,15 @@ import torch.nn.functional as F
 class CNN1DClassifier(nn.Module):
     def __init__(self, input_length, num_classes):
         super(CNN1DClassifier, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=input_length, out_channels=16, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool1d(kernel_size=2, stride=1)
+        self.maxpool = nn.MaxPool1d(kernel_size=1, stride=1)
         self.flatten = nn.Flatten()
         self.drop1 = nn.Dropout(0.5)
         # Calculate the output length after convolutions and pooling
-        conv_output_length = input_length // 8
+        conv_output_length = input_length
         self.fc1 = nn.Linear(64 * conv_output_length, 128)
         self.fc2 = nn.Linear(128, num_classes)
 #        self.softmax = nn.Softmax(dim=1)
@@ -42,10 +42,12 @@ class CNN1DClassifier(nn.Module):
 #        print("Iteration over")
         return x
 
+
 def split_dataset(dataset, train_ratio=0.8):
     train_size = int(train_ratio * len(dataset))
     test_size = len(dataset) - train_size
     return random_split(dataset, [train_size, test_size])
+
 
 
 # Initialize the model, loss function, and optimizer
@@ -62,7 +64,7 @@ features_path = "Hsapiens_features.txt"
 num_classes, input_length, dataset = cell_sampling.load_data(mtx_path, colnames_path, cells_path, rownames_path, features_path)
 print(num_classes)
 print(input_length)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+#dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -138,7 +140,8 @@ def evaluate_model(model, test_loader):
     print(f"Test Recall: {recall:.4f}")
     print(f"Test F1 Score: {f1:.4f}")
 
-#train_loader, test_loader = split_dataset(dataset)
-train_model(model, dataloader, criterion, optimizer, num_epochs=10)
-#evaluate_model(model, test_loader)
+train_loader, test_loader = split_dataset(dataset)
+train_loader, test_loader = DataLoader(train_loader, batch_size=32, shuffle=True), DataLoader(test_loader, batch_size=32, shuffle=True)
+train_model(model, train_loader, criterion, optimizer, num_epochs=10)
+evaluate_model(model, test_loader)
 
