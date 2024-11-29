3d79f259adafba4c7274fe2c99bfeb7ee3808e3b
LeonidU <leoniduroshlev@gmail.com>
2024-11-29 22:34:18 +0300

    Fix Bugs

--- model.py
+++ model.py
@@ -80,29 +80,16 @@ class SingleCellResNet(nn.Module):
 
 class FocalLoss(nn.Module):
     def __init__(self, alpha=1, gamma=2, reduction='mean'):
-        """
-        :param alpha: Вес, отвечающий за важность положительных примеров (может быть использован для борьбы с дисбалансом классов).
-        :param gamma: Параметр фокусировки, уменьшающий вклад легко классифицируемых примеров.
-        :param reduction: Способ агрегации лосса - 'none', 'mean' или 'sum'.
-        """
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.reduction = reduction
 
     def forward(self, inputs, targets):
-        """
-        :param inputs: Логиты, выходящие из сети (размерность [batch_size, num_classes]).
-        :param targets: Истинные метки классов (размерность [batch_size]).
-        """
-        # Применяем softmax к входным данным для получения вероятностей
         probs = F.softmax(inputs, dim=1)
-        # Берем вероятности для истинного класса
         probs = probs.gather(1, targets.view(-1, 1)).squeeze(1)
-        # Вычисляем фокальную потерю
         focal_weight = (1 - probs) ** self.gamma
         loss = -self.alpha * focal_weight * torch.log(probs)
-        
         if self.reduction == 'mean':
             return loss.mean()
         elif self.reduction == 'sum':
@@ -111,73 +98,6 @@ class FocalLoss(nn.Module):
             return loss
 
 
-
-"""
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(ResidualBlock, self).__init__()
-        super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=5, stride=stride, padding=2)
-        self.bn1 = nn.BatchNorm1d(out_channels)
-        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=5, stride=1, padding=2)
-        self.bn2 = nn.BatchNorm1d(out_channels)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0),
-                nn.BatchNorm1d(out_channels)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-class SingleCellResNet(nn.Module):
-    def __init__(self, input_features, num_classes):
-        super(SingleCellResNet, self).__init__()
-        # Embedding layer to reduce dimensionality
-        self.embedding_layer = nn.Linear(input_features, 512)
-        self.input_layer = nn.Sequential(
-            nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
-        )
-
-        self.layer1 = self._make_layer(64, 64, num_blocks=2, stride=1)
-        self.layer2 = self._make_layer(64, 128, num_blocks=2, stride=2)
-        self.layer3 = self._make_layer(128, 256, num_blocks=2, stride=2)
-        self.layer4 = self._make_layer(256, 512, num_blocks=2, stride=2)
-
-        self.global_avg_pool = nn.AdaptiveAvgPool1d(1)
-        self.fc = nn.Linear(512, num_classes)
-
-    def _make_layer(self, in_channels, out_channels, num_blocks, stride):
-        layers = []
-        layers.append(ResidualBlock(in_channels, out_channels, stride))
-        for _ in range(1, num_blocks):
-            layers.append(ResidualBlock(out_channels, out_channels))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.embedding_layer(x)  # Map to lower-dimensional space
-        x = x.unsqueeze(1)  # Add channel dimension if necessary
-        x = self.input_layer(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.global_avg_pool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
-"""
 
 def split_dataset(dataset, train_ratio=0.8):
     train_size = int(train_ratio * len(dataset))
@@ -260,10 +180,6 @@ def evaluate_model(model, test_loader, size):
     # Convert outputs to predicted labels
     all_outputs_np = np.array(all_outputs)
     all_labels_np = np.array(all_labels)
-#    predicted_labels = np.argmax(all_outputs_np, axis=1)
-#    np.savetxt("all_outputs_np.csv", all_outputs_np, delimiter=",")
-#    np.savetxt("all_labels_np.csv", all_labels_np, delimiter=",")
-#    np.savetxt("predicted_labels.csv", predicted_labels, delimiter=",")
 
     # Calculate metrics
     f1 = f1_score(all_labels_np, all_outputs_np, average='weighted')  # You can change 'weighted' to 'micro', 'macro', etc.
@@ -288,14 +204,9 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, input_cl
         running_loss = 0.0
         for inputs, labels in train_loader:
             inputs, labels = inputs.float().to(device), labels.to(device)
- #           print("input shape")
- #           print(inputs.shape)
- #           print(labels.shape)
             optimizer.zero_grad()
             assert not torch.isnan(inputs).any(), "Inputs contain NaN values"
             outputs = model(inputs)
-#            print(inputs)
-#            print(outputs)
             loss = criterion(outputs, labels)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
